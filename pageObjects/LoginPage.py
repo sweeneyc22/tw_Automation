22f88819022342f8dd95b7779b8efcfdb507cb29
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-01-27 15:52:17 -0800

    Update LoginPage.py

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -1,36 +1,12 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class LoginPage:
+from pageObjects.BaseElement import BaseElement
+
+
+class LoginPage(BasePage):
+
+    url = 'https://tribalwars.co.uk'
+
     def __init__(self, driver):
         self.driver = driver
         self.url = 'https://www.tribalwars.co.uk/'
-
-
-    def go(self):
-        self.driver.get(self.url)
-
-    def writeUsername(self, text):
-        box = self.driver.find_element(By.CSS_SELECTOR, "#user")
-        box.clear()
-        box.send_keys(text)
-        return None
-
-    def writePassword(self, text):
-        box = self.driver.find_element(By.CSS_SELECTOR, "#password")
-        box.send_keys(text)
-        return None
-
-    def getUserText(self):
-        text = self.driver.find_element(By.CSS_SELECTOR, "#user")
-        textValue = text.get_attribute('value')
-        return textValue
-
-    def getPassText(self):
-        text = self.driver.find_element(By.CSS_SELECTOR, "#password")
-        textValue = text.get_attribute('value')
-        return textValue
-
-    def clickLoginButton(self):
-        loginButton = self.driver.find_element(By.CLASS_NAME, "btn-login")
-        
